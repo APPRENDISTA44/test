9d1c94ad4a9f8a410970477e1f102d9e1b63b9b5
Tommaso Venza <tommasovenza@gmail.com>
2020-09-21 10:33:00 +0200

    cambiato titolo di prova

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    ciao ciao
                 </div>
 
                 <div class="links">
