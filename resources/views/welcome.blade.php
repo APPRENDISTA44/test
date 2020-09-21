7899e840025c6ae5148ef99b669dd09da9747c63
Al-1117 <alioune1117@gmail.com>
2020-09-21 11:13:07 +0200

    modificato titolo di prova

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ciao ragazzi
+                    ciao da Al!
                 </div>
 
                 <div class="links">
