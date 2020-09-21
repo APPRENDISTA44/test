0c07cb7ae642c50b0b55a02479da1e5278ed164b
Al-1117 <alioune1117@gmail.com>
2020-09-21 11:55:09 +0200

    AlHub

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -92,7 +92,7 @@
                     <a href="https://nova.laravel.com">Al</a>
                     <a href="https://forge.laravel.com">Forge</a>
                     <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/laravel/laravel">AlHub</a>
                 </div>
             </div>
         </div>
