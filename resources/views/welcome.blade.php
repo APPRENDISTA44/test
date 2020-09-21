4e2e46a307cf48cd7e6f1976d8cc56b0841ac9e4
lindam <linda.minotti2@gmail.com>
2020-09-21 10:49:57 +0200

    Cambiato titolo di prova, Linda.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    ciao ciao
+                    ciao ragazzi
                 </div>
 
                 <div class="links">
